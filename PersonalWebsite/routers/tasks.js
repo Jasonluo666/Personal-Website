cf6eb48352782dfd7cfb049df4adca53fdb176a6
Jasonluo996 <jasonluo996@gmail.com>
2019-10-14 13:55:18 -0400

    debug

--- PersonalWebsite/routers/tasks.js
+++ PersonalWebsite/routers/tasks.js
@@ -81,8 +81,7 @@ fs = require('fs');
 router.get('/img/:img_name', (req, res, next) => {
     fs.readFile(img_path + req.params.img_name, function(err, data) {
         if (err) throw err; // Fail if the file can't be read.
-
-        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        
         res.end(data); // Send the file data to the browser.
       });
 });
